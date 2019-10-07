7089fa0c5d916916736b87935cf98d3c49d16682
Sergio <sregidor@redhat.com>
2019-10-07 18:13:44 +0200

    Add new test and jenkins reporting

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export JUNIT_OUTPUT_DIR=junit_output
+export JUNIT_FAIL_ON_IGNORE=True
+export JUNIT_TEST_CASE_PREFIX=test_
+export DEFAULT_ROLES_PATH=${DIR}/roles
+
+
+ansible-playbook ${DIR}/main.yml
